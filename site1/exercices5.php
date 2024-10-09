b55a889b6e819ea6914cd9f7f4d30a145649ce0d
AICHI ADEL <aichi_a@JEAN-ROSTAND.lan>
2024-10-09 15:31:38 +0200

    exercices5

--- site1/exercices5.php
+++ site1/exercices5.php
@@ -3,9 +3,10 @@
 
 function concatenationAvecEspace(string $arg3, string $arg4){
 
-return $arg3.$arg4;
+    return $arg3. " " .$arg4;
 }
-echo concatenation("Ngolo","Kante")
+echo concatenationAvecEspace("Ngolo", "Kante")
  
 
 ?>
+
